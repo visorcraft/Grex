f90775f0eedea76b06e4f027295a1ab15a60f6e3
VisorCraft LLC <thomas@visorcraft.com>
2026-06-03 08:54:45 -0500

    Update version to 1.3.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
 [assembly: SupportedOSPlatform("windows10.0.17763.0")]
 [assembly: InternalsVisibleTo("Grex.Tests")]
 
